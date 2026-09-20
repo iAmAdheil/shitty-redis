e9dab14116f84e70890c2d560b918250a40cfd00
iAmAdheil <adheilgupta@gmail.com>
2026-09-20 19:31:08 +0530

    chore: init stream

--- app/structures/stream/stream.go
+++ app/structures/stream/stream.go
@@ -1,15 +1,29 @@
 package stream
 
+import (
+	"github.com/codecrafters-io/redis-starter-go/app/structures/radix"
+)
+
 type Id struct {
 	ms  uint64
 	seq uint64
 }
 
 type Stream struct {
-	// Rax               map[Id]*listpack.Listpack
+	Rax               *radix.RaxNode
 	Length            int
-	LastId            Id
-	MaxDeletedEntryId Id
+	LastId            *Id
+	MaxDeletedEntryId *Id
 	EntriesAdded      int
 	// cgroups
+}
+
+func NewStream() *Stream {
+	return &Stream{
+		Rax:               radix.New([]byte{}, nil), // root node
+		Length:            0,                        // count of existing entries
+		LastId:            nil,                      // last inserted id
+		MaxDeletedEntryId: nil,
+		EntriesAdded:      0, // total entries added, including deleted ones (History)
+	}
 }
